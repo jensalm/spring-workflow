f623996d69fbd66b5f662f866b06c89158ff3de5
Jens Alm <jens@ulmus.org>
2014-03-05 17:52:25 -0500

    Update Task.java

--- src/main/java/com/captechconsulting/workflow/stereotypes/Task.java
+++ src/main/java/com/captechconsulting/workflow/stereotypes/Task.java
@@ -4,14 +4,24 @@ import org.springframework.stereotype.Component;
 
 import java.lang.annotation.*;
 
+/**
+ * Indicates a task. The task will be a method that is called by the flow.
+ */
 @Documented
-@Target({ElementType.METHOD})
+@Target({ ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 @Component
 public @interface Task {
 
+    /**
+     * The name of the task, defaults to the method name if empty
+     * @return
+     */
     String value() default "";
 
-    String flow() default "";
-
+    /**
+     * An optional description of the task
+     * @return
+     */
+    String description() default "";
 }
