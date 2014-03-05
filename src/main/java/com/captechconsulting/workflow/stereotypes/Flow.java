97313fd61c1fc103e3598e2272526f202b9f1c13
Jens Alm <jens@ulmus.org>
2014-03-05 17:51:40 -0500

    Update Flow.java

--- src/main/java/com/captechconsulting/workflow/stereotypes/Flow.java
+++ src/main/java/com/captechconsulting/workflow/stereotypes/Flow.java
@@ -4,14 +4,31 @@ import org.springframework.stereotype.Component;
 
 import java.lang.annotation.*;
 
+/**
+ * Names and connects tasks. Flows can be split over several
+ * classes as long as the name is the same.
+ */
 @Documented
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Component
 public @interface Flow {
 
-    String value() default "";
+    /**
+     * Name of the flow, will default to the simple class name if not specified
+     * @return
+     */
+    String[] name() default { };
 
-    Class[] types() default {};
+    /**
+     * The types of arguments the tasks in this flow will have.
+     * @return
+     */
+    Class[] types() default { };
 
+    /**
+     * Optional description of the flow
+     * @return
+     */
+    String description() default "";
 }
