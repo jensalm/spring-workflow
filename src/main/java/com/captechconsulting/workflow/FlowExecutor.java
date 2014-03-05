8965a919025c0ac6b89c2a134527a5a0505c5d6f
Jens Alm <jens@ulmus.org>
2014-03-05 17:55:04 -0500

    Update FlowExecutor.java

--- src/main/java/com/captechconsulting/workflow/FlowExecutor.java
+++ src/main/java/com/captechconsulting/workflow/FlowExecutor.java
@@ -5,18 +5,41 @@ import com.captechconsulting.workflow.engine.FlowAdapter;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Entry point for all workflow interaction. This class should be a
+ * singleton and it should be autowired in to all classes that need
+ * to kick off workflows.
+ */
 public class FlowExecutor extends HashMap<String, FlowAdapter> {
 
-    public FlowExecutor(Map<String, FlowAdapter> flows) {
-        super(flows);
+    /**
+     * Creates a new FlowExecutor
+     */
+    public FlowExecutor() {
+        super();
     }
 
-    public boolean execute(String name, Object... types) throws Throwable {
+    /**
+     * Executes the named flow by getting the FlowAdapter that
+     * corresponds to the name and calling the start method.
+     * @param name
+     * @param types
+     * @return if the last task return true or false
+     * @throws WorkflowException
+     * @see FlowAdapter
+     * @see TaskAdapter
+     */
+    public boolean execute(String name, Object... types) throws WorkflowException {
         FlowAdapter flowAdapter = get(name);
-        if (flowAdapter == null ) {
-            throw new Throwable("No flow named: "+name);
-        }
         return flowAdapter.start(types);
     }
 
+    @Override
+    public FlowAdapter get(Object key) {
+        FlowAdapter flowAdapter = super.get(key);
+        if (flowAdapter == null) {
+            throw new RuntimeException("No flow named: " + key.toString());
+        }
+        return flowAdapter;
+    }
 }
